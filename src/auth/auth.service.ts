58bfbc86923fb7a6533cb23645b2d5b02c5487ed
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-05-09 09:41:07 +0700

    add register functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
+import * as argon from 'argon2';
+import { Body, ForbiddenException } from '@nestjs/common';
+import { UserInput } from 'src/user/dto/user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(@Body() input: UserInput) {
+    const hash = await argon.hash(input.password);
+
+    try {
+      return await this.prisma.user.create({
+        data: {
+          username: input.username,
+          hash_password: hash,
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credential already taken');
+        }
+      }
+      throw error;
+    }
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,39 +1,9 @@
-import {
-  Body,
-  Controller,
-  ForbiddenException,
-  Get,
-  Post,
-} from '@nestjs/common';
-import { UserInput } from './dto/user.dto';
-import * as argon from 'argon2';
+import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Controller('users')
 export class UserController {
   constructor(private prisma: PrismaService) {}
-
-  @Post()
-  async createUser(@Body() input: UserInput) {
-    const hash = await argon.hash(input.password);
-
-    try {
-      return await this.prisma.user.create({
-        data: {
-          username: input.username,
-          hash_password: hash,
-        },
-      });
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException('Credential already taken');
-        }
-      }
-      throw error;
-    }
-  }
 
   @Get()
   getAllUsers() {
