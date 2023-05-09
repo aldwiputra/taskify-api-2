5c106a2fcb0da746794787cc3ca651e6e6d4721f
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-05-09 13:32:19 +0700

    add esmoduleinterop and mongo store for session

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as session from 'express-session';
-import * as passport from 'passport';
+import session from 'express-session';
+import passport from 'passport';
+import MongoStore from 'connect-mongo';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,9 +15,17 @@ async function bootstrap() {
 
   app.use(
     session({
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGOSTORE_URL,
+        dbName: 'session-db',
+        ttl: 7 * 24 * 60 * 60,
+      }),
       secret: process.env.SECRET,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      },
     }),
   );
   app.use(passport.initialize());
